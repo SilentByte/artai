c4186edda1eeab7caf330231673e2fbfcf1f9bcc
Rico A. Beti <rico.beti@silentbyte.com>
2021-08-02 01:34:44 +0800

    Improve animation

--- renderer/scenes/artai/render_item.gd
+++ renderer/scenes/artai/render_item.gd
@@ -1,24 +1,24 @@
 extends Sprite
 
-var _started = false
+onready var fade_curve = preload("res://scenes/artai/fade_curve.tres")
+
+var _start_time = 0
 var _ttl = 0.0
-var _fade_fraction = 1.0
 var _movement = Vector2.ZERO
 var _rotation = 0.0
 var _scale = 0.0
 var _smooth_width_fraction = 0.1
-var _smooth_width = 0.0
+var _smooth_width = 0.5
 
 
 # TODO: IMPLEMENT FADE-OUT.
-func animate(ttl: float, fade_fraction: float, movement: Vector2, rotation: float, scale: float, smooth_width_fraction: float) -> void:
-	_started = true
+func animate(ttl: float, movement: Vector2, rotation: float, scale: float, smooth_width_fraction: float) -> void:
+	_start_time = OS.get_ticks_usec()
 	_ttl = ttl
-	_fade_fraction = fade_fraction
 	_movement = movement
 	_rotation = rotation
 	_scale = scale
-	_smooth_width = 0.5
+	_smooth_width = 0.4
 	_smooth_width_fraction = smooth_width_fraction
 	self_modulate.a = 0.0
 
@@ -26,14 +26,20 @@ func animate(ttl: float, fade_fraction: float, movement: Vector2, rotation: floa
 	queue_free()
 
 
+func _age() -> float:
+	return (OS.get_ticks_usec() - _start_time) / 1_000_000.0
+
+
 func _process(delta: float) -> void:
-	if not _started:
+	if not _start_time:
 		return
 
 	position += _movement * delta
 	rotation_degrees += _rotation * delta
 	scale += Vector2(_scale * delta, _scale * delta)
-	self_modulate.a += clamp(delta * (1.0 / _ttl * _fade_fraction), 0.0, 1.0)
+
+	self_modulate.a = fade_curve.interpolate(_age() / _ttl)
 
 	_smooth_width -= clamp(delta * (1.0 / _ttl * _smooth_width_fraction), 0.0, 1.0)
+	_smooth_width = clamp(_smooth_width, 0.1, 0.5)
 	get_material().set_shader_param("smooth_width", _smooth_width)
--- renderer/scenes/artai/renderer.gd
+++ renderer/scenes/artai/renderer.gd
@@ -11,28 +11,28 @@ func _ready() -> void:
 	# TODO: IMPLEMENT.
 	for i in range(10000):
 		_add_item()
-		yield(get_tree().create_timer(4), "timeout")
+		yield(get_tree().create_timer(5), "timeout")
 
 
 func _add_item() -> void:
 	var item = item_scene.instance()
 
-	item.position += Vector2(1024, 1024)
+	item.position = Vector2(1024, 1024)
 	item.position.x += rand_range(-256, 256)
 	item.position.y += rand_range(-256, 256)
+	item.rotation_degrees += rand_range(-180, 180)
+	item.scale = Vector2(1.5, 1.5)
 	item.texture = Curator.next().texture
 
-	var item_ttl = rand_range(15, 20)
-	var item_fade_fraction = rand_range(5, 10)
+	var item_ttl = rand_range(20, 40)
 	var item_movement = Vector2(rand_range(-64, 64), rand_range(-64, 64))
-	var item_rotation = rand_range(-20, 20)
+	var item_rotation = rand_range(-3, 3)
 	var item_scale = rand_range(0.1, 0.5)
-	var item_smooth_width_fraction = rand_range(0.1, 0.5)
+	var item_smooth_width_fraction = rand_range(1.0, 2.0)
 
 	content.add_child(item)
 	item.animate(
 		item_ttl,
-		item_fade_fraction,
 		item_movement,
 		item_rotation,
 		item_scale,
