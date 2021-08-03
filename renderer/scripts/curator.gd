f96135d8e8bc400ef45d3d48a4633e1f3a35dafb
Rico A. Beti <rico.beti@silentbyte.com>
2021-08-03 18:16:26 +0800

    Fix resource loading when exported

--- renderer/scripts/curator.gd
+++ renderer/scripts/curator.gd
@@ -1,34 +1,43 @@
 extends Node
 
-const DEFAULT_ART_DIR = "res://gfx/default_art/"
-var _default_art_file_names: PoolStringArray
-
-
-func _ready() -> void:
-	_default_art_file_names = _list_default_art_file_names()
-
-
-func _list_default_art_file_names() -> PoolStringArray:
-	var dir = Directory.new()
-	dir.open(DEFAULT_ART_DIR)
-	dir.list_dir_begin()
-
-	var file_names = PoolStringArray()
-	while true:
-		var file_name = dir.get_next()
-		if not file_name:
-			break
-
-		if file_name.ends_with(".jpg"):
-			file_names.append(DEFAULT_ART_DIR + file_name)
-
-	dir.list_dir_end()
-
-	return file_names
+var _default_art_textures = [
+	"res://gfx/default_art/01.jpg",
+	"res://gfx/default_art/02.jpg",
+	"res://gfx/default_art/03.jpg",
+	"res://gfx/default_art/04.jpg",
+	"res://gfx/default_art/05.jpg",
+	"res://gfx/default_art/06.jpg",
+	"res://gfx/default_art/07.jpg",
+	"res://gfx/default_art/08.jpg",
+	"res://gfx/default_art/09.jpg",
+	"res://gfx/default_art/10.jpg",
+	"res://gfx/default_art/11.jpg",
+	"res://gfx/default_art/12.jpg",
+	"res://gfx/default_art/13.jpg",
+	"res://gfx/default_art/14.jpg",
+	"res://gfx/default_art/15.jpg",
+	"res://gfx/default_art/16.jpg",
+	"res://gfx/default_art/17.jpg",
+	"res://gfx/default_art/18.jpg",
+	"res://gfx/default_art/19.jpg",
+	"res://gfx/default_art/20.jpg",
+	"res://gfx/default_art/21.jpg",
+	"res://gfx/default_art/22.jpg",
+	"res://gfx/default_art/23.jpg",
+	"res://gfx/default_art/24.jpg",
+	"res://gfx/default_art/25.jpg",
+	"res://gfx/default_art/26.jpg",
+	"res://gfx/default_art/27.jpg",
+	"res://gfx/default_art/28.jpg",
+	"res://gfx/default_art/29.jpg",
+	"res://gfx/default_art/30.jpg",
+	"res://gfx/default_art/31.jpg",
+	"res://gfx/default_art/32.jpg",
+]
 
 
 func _random_default_art() -> CuratorItem:
-	var file_name = _default_art_file_names[int(rand_range(0, len(_default_art_file_names) - 1))]
+	var file_name = _default_art_textures[int(rand_range(0, len(_default_art_textures) - 1))]
 	return CuratorItem.new("ArtAI", "", load(file_name))
 
 
